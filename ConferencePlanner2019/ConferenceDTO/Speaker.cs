50b8cda92937acf2897bd76d6a26735e3ccbcee1
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-29 18:37:30 -0400

    Adding response DTO classes
    AttendeeResponse SessionResponse ConferenceResponse TrackResponse TagResponse

--- ConferencePlanner2019/ConferenceDTO/Speaker.cs
+++ ConferencePlanner2019/ConferenceDTO/Speaker.cs
@@ -1,25 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.EntityFrameworkCore.Design;
 
 namespace ConferenceDTO
 {
     public class Speaker
     {
-       public int ID { get; set; }
+        public int ID { get; set; }
 
-       [Required]
-       [StringLength(200)]
-       public string Name { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
 
-       [StringLength(4000)]
-       public string Bio { get; set; }
+        [StringLength(4000)]
+        public string Bio { get; set; }
 
-       [StringLength(1000)]
-       public virtual string WebSite { get; set; }
+        [StringLength(1000)]
+        public virtual string WebSite { get; set; }
     }
 }
