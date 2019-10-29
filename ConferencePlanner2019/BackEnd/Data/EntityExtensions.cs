2c83dc3cec2cb7eaae66b52854dac9bc51ee45c2
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-28 23:33:13 -0400

    Updating Speakers controller api

---
+++ ConferencePlanner2019/BackEnd/Data/EntityExtensions.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+
+namespace BackEnd.Data
+ {
+     public static class EntityExtensions
+     {
+         public static ConferenceDTO.SpeakerResponse MapSpeakerResponse(this Speaker speaker) =>
+             new ConferenceDTO.SpeakerResponse
+             {
+                 ID = speaker.ID,
+                 Name = speaker.Name,
+                 Bio = speaker.Bio,
+                 WebSite = speaker.WebSite,
+                 Sessions = speaker.SessionSpeakers?
+                     .Select(ss =>
+                         new ConferenceDTO.Session
+                         {
+                             ID = ss.SessionId,
+                             Title = ss.Session.Title
+                         })
+                     .ToList()
+             };
+     }
+ }
