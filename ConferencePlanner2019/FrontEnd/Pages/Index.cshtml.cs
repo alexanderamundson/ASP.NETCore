4420572fa980ffdab1d5bed438804d50a40891f0
Alexander Amundson <alex.amundson987@gmail.com>
2019-11-01 21:19:51 -0400

    Connected FE to BE port to - Render home page sessions list

--- ConferencePlanner2019/FrontEnd/Pages/Index.cshtml.cs
+++ ConferencePlanner2019/FrontEnd/Pages/Index.cshtml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FrontEnd.Services;
+using ConferenceDTO;
+
 
 namespace FrontEnd.Pages
 {
